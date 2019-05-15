3789362c29b79568f244843e5deeab793351ec97
cluster <clusternlh@gmail.com>
2019-05-16 01:48:51 +0800

    Update Problem_3.py

--- Problem_3.py
+++ Problem_3.py
@@ -8,3 +8,36 @@ def find_number(x= 600851475143 ):
         max_ = i
   print(max_)
 find_number()
+
+
+"""
+two optimization consideration : [1.] 2 and fact>3  [2.] fact>3 but fact must less than $sqrt{n}$
+
+n=”the evil big number”
+if n mod 2=0
+ then
+ lastFactor=2
+ n=n div 2
+ while n mod 2=0
+ n=n div 2
+else
+lastFactor=1
+factor=3
+maxFactor= n
+while n>1 and factor<=maxFactor
+if n mod factor=0
+ then
+ n=n div factor
+lastFactor=factor
+while n mod factor=0
+n=n div factor
+maxFactor= n
+ factor=factor+2
+if n=1
+ then
+output lastFactor
+ else
+ output n
+
+
+"""
