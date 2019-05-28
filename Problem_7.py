0809fa9a78b82aba015b845a9143636bcf7e2474
cluster <clusternlh@gmail.com>
2019-05-28 16:45:19 +0800

    Create Problem_7.py

---
+++ Problem_7.py
@@ -0,0 +1,17 @@
+from math import floor
+
+n = 10 ** 6
+a = [1 for i in range(n + 1)]
+# a[2] = 1
+# a[3] = 1
+p = []
+for i in range(2, n):
+    if a[i]:
+        k = floor(n / i)
+        for j in range(1, k + 1):
+            a[j * i] = 0
+
+        p.append(i)
+
+print(len(p))
+print(p[10**4])
