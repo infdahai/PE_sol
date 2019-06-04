738ede39a47652415c628fca10cce481274e1a3f
cluster <clusternlh@gmail.com>
2019-06-04 17:39:37 +0800

    Update Problem_11.py

--- Problem_11.py
+++ Problem_11.py
@@ -1,4 +1,4 @@
-input = '''08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+input_2 = '''08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
 52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
@@ -19,20 +19,105 @@ input = '''08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48'''
 
-input_list = input.split('\n')
-max_val = 1788696
+input_1 = '''08 02 22 97
+49 49 99 40
+81 49 31 73
+52 70 95 23'''
 
 
-# 如何遍历列表计算某个值，并且重复次数最小。感觉有点想不清楚。
+'''
+TODO:
+    可以考虑根据分块大小，建立前缀积和后缀积，从而快速算出 水平或竖直方向的最大值。消耗是三次n乘法。
+'''
+
+input_list = input_2.split('\n')
+int_list = []
+for l_j in input_list:
+    l_j = l_j.split(' ')
+    l1 = list(map(int, l_j))
+    int_list.append(l1)
+max_list =[]
+#max_val = 1788696
+
 
 def leftToRight(a: list) -> int:
     l = len(a)
     val = 0
-    for i in range(0, l - 4):
+    for i in range(0, l - 3):
         mul = 1
         for j in range(4):
-            mul *= a[i + j]
-        val = mul if val < mul else val
+            if (a[i + j]!=0):
+                mul *= a[i + j]
+            else:
+                mul = 0
+                break
+        val = max(val,mul)
+    return val
+
 
 max_leftToRight = 0
+for list_left in int_list:
+    val = leftToRight(list_left)
+    max_leftToRight = max(max_leftToRight, val)
+max_list.append(max_leftToRight)
+
+def upToDown(a: list) -> int:  # a -> int_list
+    l1 = len(a)
+    l2 = len(a[0])
+    val = 0
+    for i in range(l1 - 3):
+        mul1 = 1
+        for j in range(l2):
+            mul2 = 1
+            for k in range(4):
+                if(a[i + k][j]!=0):
+                    mul2 *= a[i + k][j]
+                else:
+                    mul2 = 0
+                    break
+            mul1 = max(mul1, mul2)
+        val = max(mul1, val)
+    return val
+
+max_upToDown = upToDown(int_list)
+max_list.append(max_upToDown)
+
+def leftDiagonRight(a:list) -> int : # a -> int_list
+    l = len(a)
+    val = 0
+    for i in range(l-3):
+        mul1 = 1
+        for j in range(l-3):
+            mul2 = 1
+            for k in range(4):
+                if(a[i+k][j+k]!=0):
+                    mul2 *= a[i+k][j+k]
+                else:
+                    mul2 = 0
+                    break
+            mul1 = max(mul1, mul2)
+        val = max(mul1, val)
+    return val
+
+def rightDiagonLeft(a:list) -> int :
+    l = len(a)
+    val = 0
+    for i in range(l - 3):
+        mul1 = 1
+        for j in range(3,l ):
+            mul2 = 1
+            for k in range(4):
+                if (a[i + k][j - k] != 0):
+                    mul2 *= a[i + k][j - k]
+                else:
+                    mul2 = 0
+                    break
+            mul1 = max(mul1, mul2)
+        val = max(mul1, val)
+    return val
+
+max_list.append(leftDiagonRight(int_list))
+max_list.append(rightDiagonLeft(int_list))
+print(max(max_list))
+
 
